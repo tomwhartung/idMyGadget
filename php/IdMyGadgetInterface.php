e12bc386e5b0aa509d223e41f90b14f7b13016cd
Tom W Hartung <tomwhartung@gmail.com>
2015-04-23 15:55:41 -0600

    adding a new interface that should work with the different libraries I have found.

---
+++ php/IdMyGadgetInterface.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Defines methods and gadget types, etc.
+ */
+
+interface IdMyGadgetInterface
+{
+	public function displayDeviceData();
+
+	const GADGET_TYPE_UNRECOGNIZED = "unrecognized";
+	const GADGET_TYPE_DESKTOP_BROWSER = "desktop";
+	const GADGET_TYPE_TABLET = "tablet";
+	const GADGET_TYPE_PHONE = "phone";
+
+	const GADGET_MODEL_UNRECOGNIZED = "unrecognized";
+	const GADGET_MODEL_NAME_NOT_SET = "model_name_not_set";
+	const GADGET_MODEL_ANDROID_TABLET = "androidTablet";
+	const GADGET_MODEL_APPLE_TABLET = "iPad";
+
+	const GADGET_MODEL_ANDROID_PHONE = "androidPhone";
+	const GADGET_MODEL_APPLE_PHONE = "iPhone";
+
+	const GADGET_BRAND_UNRECOGNIZED = "unrecognized";
+	const GADGET_BRAND_NAME_NOT_SET = "brand_name_not_set";
+	const GADGET_BRAND_ANDROID = "Android";
+	const GADGET_BRAND_APPLE = "Apple";
+}
+
+?>
