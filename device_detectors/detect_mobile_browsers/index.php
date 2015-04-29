2c0c076fea3d39b7594d367d67f30a36a586891f
Tom W Hartung <tomwhartung@gmail.com>
2015-04-29 12:54:24 -0600

    Added real content to device_detectors/detect_mobile_browsers/index.php .

--- device_detectors/detect_mobile_browsers/index.php
+++ device_detectors/detect_mobile_browsers/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang='en'>
 <?php
-$pageTitle = 'IdMyGadget Mobile-Detect Demos';
+$pageTitle = 'detect_mobile_browsers';
 ?>
 
 <head>
@@ -23,16 +23,23 @@ print "<h2></h2>";
 ?>
 
 <div id="idMyGadget">
- <h3>IdMyGadget Mobile-Detect Demos:</h3>
+ <h3>detect_mobile_browsers demos:</h3>
  <dl>
   <dt><a href="README.md" target="_blank">README.md</a></dt>
-  <dd>The idMyGadget Mobile-Detect README file contains instructions on how to set up Mobile-Detect.
-    The formatted <a href="https://github.com/tomwhartung/idMyGadget/blob/master/README.md">version on github</a> is more readable.</dd>
-  <dt><a href="index.php?displayDeviceData=true">index.php?displayDeviceData=true</a></dt>
-  <dd>Displays the gadget types that idMyGadget has deduced from the key capabilities obtained from Tera-Wurfl</dd>
+  <dd>Contains instructions on how to enable this option.
+    <a href="https://github.com/tomwhartung/idMyGadget/blob/master/device_detectors/detect_mobile_browsers/README.md"
+       target="_blank">The formatted version on github</a>
+    is more readable.</dd>
+  <dt><a href="rawDemo.php">rawDemo.php</a></dt>
+  <dd>Demos detect_mobile_browsers device detection, <strong>without</strong> using the IdMyGadget API.</dd>
+  <dt><a href="idMyGadgetDemo.php">idMyGadgetDemo.php</a></dt>
+  <dd>Demos detect_mobile_browsers device detection, using the IdMyGadget API.</dd>
   <dt><a href=""></a></dt>
   <dd></dd>
  </dl>
+ <hr />
+ <p class="centered"><a href="..">Back to device_detectors</a></dt>
+ <hr />
 </div> <!-- idMyGadget-->
 
  </div> <!-- content -->
