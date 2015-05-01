749289c06932a128280e8d8a2d474493b6407f95
Tom W Hartung <tomwhartung@gmail.com>
2015-05-01 13:53:42 -0600

    Added tablet specific content to contingentContent.php .

--- device_detectors/contingentContent.php
+++ device_detectors/contingentContent.php
@@ -13,10 +13,21 @@ function contingentContent( $deviceData )
     print 'for example.';
     print '</p>';
   }
+  else if ( $deviceData['gadgetType'] === IdMyGadget::GADGET_TYPE_TABLET )
+  {
+    print '<p>';
+    print 'This is content for tablets only.  ';
+    print '</p>';
+    print '<p>';
+    print 'You might want this content to link to ';
+    print '<a href="https://en.wikipedia.org/wiki/Computer_security">a page about computer security</a>, ';
+    print 'for example.';
+    print '</p>';
+  }
   else
   {
     print '<p>';
-    print 'This is content for browsers that are not on a phone.  ';
+    print 'This is content for browsers that are not on a mobile device.  ';
     print '</p>';
     print '<p>';
     print 'You might want this content to link to ';
