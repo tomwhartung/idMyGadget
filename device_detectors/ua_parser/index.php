b3ea9cea7d4d25c6a1b6fba44de938ad4f0e1f66
Tom W Hartung <tomwhartung@gmail.com>
2015-05-03 17:21:09 -0600

    Add minimal "TBD" index.php file to ua_parser.

---
+++ device_detectors/ua_parser/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang='en'>
+<?php
+$detectorName = 'ua_parser';
+?>
+
+<head>
+  <title><?php print $detectorName; ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" type="text/css" href="../../css/allDevices.css" />
+  <link rel="stylesheet" type="text/css" href="../../css/basicMediaQueries.css" />
+  <!--[if IE]>
+    <link rel="stylesheet" type="text/css" href="../../css/device/explorer.css" media="all" />
+  <![endif]-->
+</head>
+
+<body>
+  <div id="container">
+<?php
+print "<h1>$detectorName</h1>";
+print "<div id='content'>";
+?>
+
+<div id="idMyGadget">
+ <h3>IdMyGadget ua_parser Demos:</h3>
+ <dl>
+  <dt><a href=""></a>TBD.</dt>
+  <dd></dd>
+ </dl>
+ <hr />
+ <p class="centered"><a href="..">Back</a></dt>
+ <hr />
+</div> <!-- idMyGadget-->
+
+ </div> <!-- content -->
+</div> <!-- container -->
+</body>
+</html>
