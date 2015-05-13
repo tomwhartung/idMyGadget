85cd496e057dc7b0982d1bd0f1a1d40e2f1413d1
Tom W Hartung <tomwhartung@gmail.com>
2015-05-12 21:48:21 -0600

    Making some final changes (I hope).

--- gadget_detectors/index.php
+++ gadget_detectors/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang='en'>
 <?php
-$pageTitle = 'IdMyGadget Demos';
+$pageTitle = 'IdMyGadget';
 ?>
 
 <head>
@@ -17,13 +17,15 @@ $pageTitle = 'IdMyGadget Demos';
 <body>
   <div id="container">
 <?php
-print "<h1>$pageTitle</h1>";
-print "<div id='content'>";
-print "<h2></h2>";
+  print "<h1>$pageTitle</h1>";
+  print "<div id='content'>";
 ?>
 
 <div id="idMyGadget">
- <h3>IdMyGadget Demos:</h3>
+ <p>The IdMyGadget Adapter API offers a common API to the device detectors listed on this page.
+  Each link takes you to a page containing links that demonstrate how to use the
+  selected detector, with and without the IdMyGadget Adapter API.
+ <h4>Gadget Detectors:</h4>
  <dl>
   <dt><a href="detect_mobile_browsers">detect_mobile_browsers</a></dt>
   <dd>A script available from the site <a href="http://detectmobilebrowsers.com" target="_blank">detectmobilebrowsers.com</a>
@@ -33,14 +35,18 @@ print "<h2></h2>";
   <dt><a href="tera_wurfl">tera_wurfl</a></dt>
   <dd>The Tera-Wurfl device detection package requires a MySql database and is released under a modified GNU licence so it is not fully open source.
    It returns hundreds of parameters for each device.</dd>
-  <dt><a href=""></a></dt>
-  <dd></dd>
+ </dl>
+ <hr />
+ <dl>
   <dt><a href="ua_parser" class="disabled">ua_parser</a></dt>
   <dd>TBD.  The repositories at
    <a href="https://github.com/ua-parser/uap-core">github.com/ua-parser/uap-core</a> and
    <a href="https://github.com/ua-parser/uap-php">github.com/ua-parser/uap-php</a>
    look promising, but I was unable to quickly get them to work, and so far the others are good enough.</dd>
  </dl>
+ <hr />
+ <p class="centered">|&nbsp;<a href="..">Back</a>&nbsp;|</p>
+ <hr />
 </div> <!-- idMyGadget-->
 
  </div> <!-- content -->
