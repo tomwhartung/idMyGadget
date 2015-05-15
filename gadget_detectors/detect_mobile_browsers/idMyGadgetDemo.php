2d53525da6be9a42edaa00da88fab3369d257ef9
Tom W Hartung <tomwhartung@gmail.com>
2015-05-15 12:51:36 -0600

    Added form to detect_mobile_browsers/idMyGadgetDemo that allows removing css from the html.

--- gadget_detectors/detect_mobile_browsers/idMyGadgetDemo.php
+++ gadget_detectors/detect_mobile_browsers/idMyGadgetDemo.php
@@ -15,13 +15,33 @@ $idMyGadget = new IdMyGadgetDetectMobileBrowsers( $debugging, $allowOverridesInU
 $deviceData = $idMyGadget->getDeviceData();
 $gadgetString = getGadgetString( $deviceData );
 $styleSheetFile = getStyleSheetFile( $deviceData );
+
+$rmAllDevicesCss = filter_input( INPUT_GET, 'rmAllDevicesCss', FILTER_SANITIZE_NUMBER_INT );
+$rmStyleSheetCss = filter_input( INPUT_GET, 'rmStyleSheetCss', FILTER_SANITIZE_NUMBER_INT );
 ?>
 
 <head>
   <title><?php print $pageTitle; ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" type="text/css" href="../../css/allDevices.css" />
-  <link rel="stylesheet" type="text/css" href="<?php print $styleSheetFile; ?>" />
+  <link rel="stylesheet" type="text/css" href="" />
+  <?php
+    if ( $rmAllDevicesCss )
+    {
+      $rmAllDevicesCssChecked = 'checked';
+    }
+    else
+    {
+      print '<link rel="stylesheet" type="text/css" href="../../css/allDevices.css" />';
+    }
+    if ( $rmStyleSheetCss )
+    {
+      $rmStyleSheetCssChecked = 'checked';
+    }
+    else
+    {
+      print '<link rel="stylesheet" type="text/css" href="' . $styleSheetFile . '" />';
+    }
+  ?>
   <!--[if IE]>
     <link rel="stylesheet" type="text/css" href="../../css/device/explorer.css" media="all" />
   <![endif]-->
@@ -36,23 +56,42 @@ $styleSheetFile = getStyleSheetFile( $deviceData );
   }
 ?>
 <div id="content">
-<h3><?php print get_class($idMyGadget); ?></h3>
-<h3><?php print $gadgetString; ?></h3>
-<div id="idMyGadget">
- <?php
-  print "<div class='output'>";
-  print "<h4>detectorUsed:" . "</h4>";
-  print "<p>" . $idMyGadget->detectorUsed . "</p>";
-
-  print "<h4>deviceData:</h4>";
-  print "<ul class='no-bullets'>" . $idMyGadget->displayDeviceData() . "</ul>";
-  print "</div> <!-- .output -->";
- ?>
-
- <hr />
- <p class="centered">|&nbsp;<a href="index.php">Back</a>&nbsp;|</p>
- <hr />
-</div> <!-- #idMyGadget-->
+  <h3><?php print get_class($idMyGadget); ?></h3>
+  <h3><?php print $gadgetString; ?></h3>
+  <div id="idMyGadget">
+    <?php
+    print "<div class='output'>";
+    print "<h4>detectorUsed:" . "</h4>";
+    print "<p>" . $idMyGadget->detectorUsed . "</p>";
+    print "<h4>deviceData:</h4>";
+    print "<ul class='no-bullets'>" . $idMyGadget->displayDeviceData() . "</ul>";
+    print "</div> <!-- .output -->";
+    ?>
+  </div> <!-- #idMyGadget-->
+  <div class="options">
+    <hr />
+    <form action="#" method="GET">
+      <fieldset>
+        <label for="rmAllDevicesCss">
+          <input type="checkbox" id="rmAllDevicesCss" name="rmAllDevicesCss" value="1"
+             <?php print $rmAllDevicesCssChecked ?> />
+          Remove allDevices.css
+        </label>
+        <label for="rmStyleSheetCss">
+          <input type="checkbox" id="rmStyleSheetCss" name="rmStyleSheetCss" value="1"
+             <?php print $rmStyleSheetCssChecked ?> />
+          Remove <?php print basename($styleSheetFile) ?>
+        </label>
+        <div class="centered">
+          <input type="submit" value="Remove Css" />
+        </div> <!-- .centered !>
+      </fieldset>
+    </form>
+  </div> <!-- .options -->
+    <hr />
+    <p class="centered">|&nbsp;<a href="index.php">Back</a>&nbsp;|</p>
+    <hr />
+  </div> <!-- .options -->
 </div> <!-- #content -->
 </div> <!-- #container -->
 </body>
