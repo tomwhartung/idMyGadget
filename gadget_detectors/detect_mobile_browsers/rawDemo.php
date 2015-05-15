884700856670c5d29c100dba6c0b7056b0c76251
Tom W Hartung <tomwhartung@gmail.com>
2015-05-15 13:27:58 -0600

    Removed unused idMyGadget div tags.

--- gadget_detectors/detect_mobile_browsers/rawDemo.php
+++ gadget_detectors/detect_mobile_browsers/rawDemo.php
@@ -22,50 +22,46 @@ require_once( 'php/detectmobilebrowser.php' );
 <div id="container">
 <h2><?php print $pageTitle; ?></h2>
 <div id="content">
+  <h3>Raw Demo Results:</h3>
+  <p class="centered">
+  <?php
+    print "usingMobilePhone = '$usingMobilePhone'";
+  ?>
+  </p>
 
-<div id="idMyGadget">
- <h3>Raw Demo Results:</h3>
- <p class="centered">
- <?php
-  print "usingMobilePhone = '$usingMobilePhone'";
- ?>
- </p>
+  <?php
+    print "<h3>Using the Results</h3>";
+    if ( $usingMobilePhone )
+    {
+      print '<p>';
+      print 'This is content for phones only.  ';
+      print '</p>';
+      print '<p>';
+      print 'You might want this content to link to ';
+      print '<a href="https://en.wikipedia.org/wiki/Mobile_security">a page about mobile security</a>, ';
+      print 'for example.';
+      print '</p>';
+    }
+    else
+    {
+      print '<p>';
+      print 'This is content for browsers that are not on a phone.  ';
+      print '</p>';
+      print '<p>';
+      print 'You might want this content to link to ';
+      print '<a href="https://en.wikipedia.org/wiki/Computer_virus">a page about computer viruses</a>, ';
+      print 'for example.';   
+      print '</p>';
+    }
+    print '<hr />';
+    print '<p>';
+    print 'This is content delivered to all browsers, regardless of the device.';
+    print '</p>';
+  ?>
 
- <?php
-  print "<h3>Using the Results</h3>";
-  if ( $usingMobilePhone )
-  {
-    print '<p>';
-    print 'This is content for phones only.  ';
-    print '</p>';
-    print '<p>';
-    print 'You might want this content to link to ';
-    print '<a href="https://en.wikipedia.org/wiki/Mobile_security">a page about mobile security</a>, ';
-    print 'for example.';
-    print '</p>';
-  }
-  else
-  {
-    print '<p>';
-    print 'This is content for browsers that are not on a phone.  ';
-    print '</p>';
-    print '<p>';
-    print 'You might want this content to link to ';
-    print '<a href="https://en.wikipedia.org/wiki/Computer_virus">a page about computer viruses</a>, ';
-    print 'for example.';   
-    print '</p>';
-  }
-  print '<hr />';
-  print '<p>';
-  print 'This is content delivered to all browsers, regardless of the device.';
-  print '</p>';
- ?>
-
- <hr />
- <p class="centered">|&nbsp;<a href="index.php">Back</a>&nbsp;|</p>
- <hr />
-</div> <!-- idMyGadget-->
-
+  <hr />
+  <p class="centered">|&nbsp;<a href="index.php">Back</a>&nbsp;|</p>
+  <hr />
 </div> <!-- content -->
 </div> <!-- container -->
 </body>
